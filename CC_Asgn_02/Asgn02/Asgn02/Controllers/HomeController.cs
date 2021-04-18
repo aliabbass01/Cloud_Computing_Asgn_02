01527e1fdeb9b16b275e8056d79638cd510b6069
aliabbass01 <bcsf17a502@pucit.edu.pk>
2021-04-18 23:06:19 +0500

    assgn02

---
+++ CC_Asgn_02/Asgn02/Asgn02/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asgn02.Models;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace Asgn02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ViewData()
+        {
+                //We will make a GET request to a really cool website...
+
+                string baseUrl = "http://52.201.152.108/news?id=bcsf17a502";
+                //The 'using' will help to prevent memory leaks.
+                //Create a new instance of HttpClient
+                using (HttpClient client = new HttpClient())
+
+                //Setting up the response...         
+
+                using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+                using (HttpContent content = res.Content)
+                {
+
+                    dynamic data = null;
+                    data = await content.ReadAsStringAsync();
+                    object result = JsonSerializer.Deserialize<List<DataModel>>(data);
+                    if (data != null)
+                    {
+                        return View(result);
+                        //Console.WriteLine(data);
+                    }
+                }
+
+                return View();
+        }
+
+        public IActionResult Load()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
---
+++ CC_Asgn_02/Asgn02/Asgn02/Models/DataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asgn02.Models
+{
+    public class DataModel
+    {
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public double Popularity { get; set; }
+        public string Quote { get; set; }
+    }
+}
